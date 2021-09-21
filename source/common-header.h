85b2218bea171ad51c763f0de36701df5bec6ce5
gmarseglia <g.marseglia.it@gmail.com>
2021-09-21 12:07:48 +0200

    #1 Done and #4 started

--- source/common-header.h
+++ source/common-header.h
@@ -11,7 +11,45 @@
 #include <netinet/ip.h>
 // Implicit declaration solved: https://stackoverflow.com/questions/11049687/something-about-inet-ntoa
 #include <arpa/inet.h>
+#include <sys/uio.h>
 
 #define fflush(stdin) while(getchar() != '\n');
 
 #define SERV_PORT 6990
+#define SIZEOF_CHAR 1
+#define SIZEOF_INT 4
+
+int send_message_to(int sockfd, char *op, char *message){
+	int message_len, byte_written;
+	struct iovec iov[2];
+
+	message_len = strlen(message);
+
+	iov[0].iov_base = op;
+	// iov[0].iov_len = SIZEOF_CHAR;
+	iov[0].iov_len = 0;
+
+	iov[1].iov_base = &message_len;
+	iov[1].iov_len = SIZEOF_INT;
+
+	if((byte_written = writev(sockfd, iov, 2)) < 0){
+		perror("Error in send_message_to on writev");
+		return -1;
+	}
+
+	#ifdef PRINT_DEBUG_FINE
+	printf("writev has written %d bytes\n", byte_written);
+	#endif
+
+	if((byte_written = write(sockfd, message, message_len)) 
+		!= message_len){
+		perror("Error in send_message_to on write");
+		return -1;
+	}
+
+	#ifdef PRINT_DEBUG_FINE
+	printf("write has written %d bytes=%s\n", byte_written, message);
+	#endif
+
+	return byte_written;
+}
